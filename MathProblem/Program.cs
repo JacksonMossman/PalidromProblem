2975851473842ab5b3aff6be81158a3cd4f474f7
jax.mossman@gmail.com <jax.mossman@gmail.com>
2020-04-27 22:20:26 -0500

    Add project files.

---
+++ MathProblem/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MathProblem
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> palindrome = new List<int>();
+            for(int i= 1;i< 10; i++)
+            {
+                for (int j = 1; j < 10;j++)
+                {
+                    for(int k = 1;k< 10;k++)
+                    {
+                       string StringI =  Convert.ToString(i);
+                        string StringJ = Convert.ToString(j);
+                        string StringK = Convert.ToString(k);
+                       string pallandrom = (StringI + StringJ + StringK + StringK + StringJ + StringI);
+                        palindrome.Add(Convert.ToInt32(pallandrom));
+                    }
+                    
+                }
+            }
+            for(int i = 0; i< palindrome.Count(); i++)
+            {
+                bool stop = false;
+                List<int> viableNumbers = new List<int>();
+                for (int b = 1; b <= palindrome.ElementAt(i); b++)
+                {            
+                    if (palindrome.ElementAt(i) % b == 0 && b > 100 && b < 1000)
+                    { 
+                        
+                        viableNumbers.Add(b);
+                       // Console.WriteLine(b + " is a factor of " + palindrome.ElementAt(i));
+                    }
+                }
+                for(int j = 0; j < viableNumbers.Count(); j++)
+                {
+                    for (int k = 0; k < viableNumbers.Count(); k++)
+                    {
+
+                        if(palindrome.ElementAt(i)==viableNumbers.ElementAt(k) * viableNumbers.ElementAt(j))
+                        {
+                            Console.WriteLine("Num1:" + Convert.ToString(viableNumbers.ElementAt(j)) +"Num 2:" + Convert.ToString(viableNumbers.ElementAt(k)));
+                            Console.ReadKey();
+                        }
+
+
+                    }
+                }
+                if(stop)
+                {
+                    Console.ReadKey();
+                }
+            }
+           
+        }
+    }
+}
